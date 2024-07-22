67c9f88f492ba55be7d71f9c355ee3133846af4a
hewei <2267849191@qq.com>
2024-07-22 10:22:09 +0800

    去掉了主界面，直接显示悬浮窗，所有功能都在悬浮窗中执行

--- app/src/main/java/com/fde/keyassist/MainActivity.java
+++ app/src/main/java/com/fde/keyassist/MainActivity.java
@@ -15,62 +15,26 @@ import com.fde.keyassist.fragment.UserProfileFragment;
 
 import org.litepal.LitePal;
 
-public class MainActivity extends Activity implements View.OnClickListener{
+public class MainActivity extends Activity{
 
     private static final String TAG = "MainActivity";
-
-    private ImageView configManageImage;
-    private ImageView userProfileImage;
-    private ImageView addEventImage;
 
 
 
     public void init(){
 //        LitePal.initialize(this); // 初始化数据库
         SQLiteDatabase db = LitePal.getDatabase();
-        configManageImage = findViewById(R.id.config_manage_image);
-        userProfileImage = findViewById(R.id.user_profile_image);
-        addEventImage = findViewById(R.id.add_event_image);
-        configManageImage.setOnClickListener(this);
-        userProfileImage.setOnClickListener(this);
-        addEventImage.setOnClickListener(this);
-        replace(new ConfigManageFragment());
     }
 
-    public void replace(Fragment fragment){
-        FragmentManager fragmentManager = getFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.base_fragment,fragment);
-        fragmentTransaction.commit();
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_main);
         init();
-
+        Intent intent = new Intent(MainActivity.this,FloatingService.class);
+        startService(intent);
+        moveTaskToBack(true);
     }
 
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()){
-            case R.id.add_event_image:
-                // 创建悬浮窗
-                Intent intent = new Intent(MainActivity.this,FloatingService.class);
-                startService(intent);
-                moveTaskToBack(true);
-                break;
-            case R.id.config_manage_image:
-                replace(new ConfigManageFragment());
-                configManageImage.setImageResource(R.drawable.config_manage_true);
-                userProfileImage.setImageResource(R.drawable.user_profile_false);
-                break;
-            case R.id.user_profile_image:
-                replace(new UserProfileFragment());
-                configManageImage.setImageResource(R.drawable.config_manage_false);
-                userProfileImage.setImageResource(R.drawable.user_profile_true);
-                break;
-        }
-    }
 }
